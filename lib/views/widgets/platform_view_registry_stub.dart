802580defe6d71cc0dafffc3c4eebbf03de03769
santhosh <santosh.kumar@simplifyvms.com>
2025-07-21 16:51:12 +0530

    trail code

---
+++ lib/views/widgets/platform_view_registry_stub.dart
@@ -0,0 +1,8 @@
+// platform_view_registry_stub.dart
+// Stub for non-web platforms
+
+class platformViewRegistry {
+  static void registerViewFactory(String viewType, dynamic cb) {
+    // Do nothing on non-web platforms
+  }
+}
